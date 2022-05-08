651e803d7f65c544b9073179f5b0c6fb266fc1ea
Olaoluwa Raji <olaoluwaraji1999@gmail.com>
2022-05-08 22:36:47 +0100

    Update Alarm_Clock_Project.ino

--- Alarm_Clock_Project/Alarm_Clock_Project.ino
+++ Alarm_Clock_Project/Alarm_Clock_Project.ino
@@ -18,45 +18,117 @@
 
 #define IR_RECEIVER   A0
 #define RESET_BUTTON  A1
-#define BUTTON1       A2
-#define BUTTON2       A3
-#define BUTTON3       2
+#define BUTTON0       A2
+#define BUTTON1       A3
+#define BUTTON2       2
 
 LiquidCrystal lcd(3,4,5,6,7,8);
-IRrecv irrecv(IR_RECEIVER);
+IRrecv irReceiver(IR_RECEIVER);
 RTC_DS3231 rtc;
+
+//Previous button states
+bool prevState[3] = {0};
+
+//Functions
+bool IsDebounced(int buttonPin)
+{
+  if(!digitalRead(buttonPin))
+  {
+    delay(15); //de-bounce delay
+    if(!digitalRead(buttonPin))
+    {
+      return true;
+    }
+  }
+  return false;
+}
+
+bool IsPressed(int buttonPin,int index)
+{
+  if(IsDebounced(buttonPin) && !prevState[index])
+  {
+    prevState[index] = true;
+    return true;
+  }
+  else if(!IsDebounced(buttonPin) && prevState[index])
+  {
+    prevState[index] = false;
+  }
+  return false;
+}
+
+void DisplayMenu(void)
+{
+  DateTime currentTime = rtc.now();
+  lcd.setCursor(2,0);
+  lcd.print("Time: ");
+  if(currentTime.hour() < 10)
+  {
+    lcd.print('0');
+    lcd.print(currentTime.hour());
+  }
+  else
+  {
+    lcd.print(currentTime.hour());
+  }
+  
+  lcd.print(':');
+  if(currentTime.minute() < 10)
+  {
+    lcd.print('0');
+    lcd.print(currentTime.minute());
+  }
+  else
+  {
+    lcd.print(currentTime.minute());
+  }
+    
+  lcd.print(':');
+  if(currentTime.second() < 10)
+  {
+    lcd.print('0');
+    lcd.print(currentTime.second());
+  }
+  else
+  {
+    lcd.print(currentTime.second());
+  }
+  lcd.setCursor(2,1);
+  lcd.print("Alarm");
+  lcd.setCursor(2,2);
+  lcd.print("Game");
+  lcd.setCursor(2,3);
+  lcd.print("Song");  
+}
 
 void setup(void) 
 {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(RESET_BUTTON,INPUT_PULLUP);
+  pinMode(BUTTON0,INPUT_PULLUP);
   pinMode(BUTTON1,INPUT_PULLUP);
   pinMode(BUTTON2,INPUT_PULLUP);
-  pinMode(BUTTON3,INPUT_PULLUP);
-  lcd.begin(20,4);
-  lcd.print("Welcome");
-  irrecv.enableIRIn();
+  irReceiver.enableIRIn();
   rtc.begin();
+  lcd.begin(20,4);
+  DisplayMenu();
 }
 
 void loop(void) 
 {
   // put your main code here, to run repeatedly:
-  if(!digitalRead(RESET_BUTTON))
+  DisplayMenu();
+  if(IsPressed(BUTTON0,0))
   {
-    Serial.println("Reset button pressed");
+    Serial.println("Button 0 -> pressed");
+  }    
+  if(IsPressed(BUTTON1,1))
+  {
+    Serial.println("Button 1 -> pressed");
   }
-  if(!digitalRead(BUTTON1))
+  if(IsPressed(BUTTON2,2))
   {
-    Serial.println("Button 1 pressed");
+    Serial.println("Button 2 -> pressed");
   }
-  if(!digitalRead(BUTTON2))
-  {
-    Serial.println("Button 2 pressed");
-  }
-  if(!digitalRead(BUTTON3))
-  {
-    Serial.println("Button 3 pressed");
-  }      
 }
